cef46f6288359923fd7dcbea713d65e8faae26de
goudasanguine <e.s.august@gmail.com>
2026-08-30 15:02:49 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
 // Minimal offline cache for Fuck Kratom.
 // Bump CACHE_NAME whenever app files change so users pick up updates.
-const CACHE_NAME = "fuck-kratom-v14";
+const CACHE_NAME = "fuck-kratom-v15";
 const APP_SHELL = [
   "./",
   "./index.html",
